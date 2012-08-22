822849dc35361dc7f8b627524ebd4149f6592f4f
davboecki <dboeken@yahoo.de>
2012-08-22 10:45:49 +0200

    Allow Jenkins to compile again

--- src/main/java/org/bukkit/map/MinecraftFont.java
+++ src/main/java/org/bukkit/map/MinecraftFont.java
@@ -11,12 +11,12 @@ public class MinecraftFont extends MapFont {
         " !\"#$%&'()*+,-./0123456789:;<=>?" +
         "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_" +
         "'abcdefghijklmnopqrstuvwxyz{|}~\u007F" +
-        "\u00C7\u00FC\u00E9\u00E2\u00E4\u00E0\u00E5\u00E7" + // Çüéâäàåç
-        "\u00EA\u00EB\u00E8\u00EF\u00EE\u00EC\u00C4\u00C5" + // êëèïîìÄÅ
-        "\u00C9\u00E6\u00C6\u00F4\u00F6\u00F2\u00FB\u00F9" + // ÉæÆôöòûù
-        "\u00FF\u00D6\u00DC\u00F8\u00A3\u00D8\u00D7\u0191" + // ÿÖÜø£Ø×ƒ
-        "\u00E1\u00ED\u00F3\u00FA\u00F1\u00D1\u00AA\u00BA" + // áíóúñÑªº
-        "\u00BF\u00AE\u00AC\u00BD\u00BC\u00A1\u00AB\u00BB";  // ¿®¬½¼¡«»
+        "\u00C7\u00FC\u00E9\u00E2\u00E4\u00E0\u00E5\u00E7" +
+        "\u00EA\u00EB\u00E8\u00EF\u00EE\u00EC\u00C4\u00C5" +
+        "\u00C9\u00E6\u00C6\u00F4\u00F6\u00F2\u00FB\u00F9" +
+        "\u00FF\u00D6\u00DC\u00F8\u00A3\u00D8\u00D7\u0191" + 
+        "\u00E1\u00ED\u00F3\u00FA\u00F1\u00D1\u00AA\u00BA" + 
+        "\u00BF\u00AE\u00AC\u00BD\u00BC\u00A1\u00AB\u00BB";  
 
     private static final int[][] fontData = new int[][] {
         /* null */  {0,0,0,0,0,0,0,0},
@@ -146,7 +146,7 @@ public class MinecraftFont extends MapFont {
         /* | */  {1,1,1,0,1,1,1,0},
         /* } */  {3,4,4,8,4,4,3,0},
         /* ~ */  {38,25,0,0,0,0,0,0},
-        /* ⌂ */  {0,0,4,10,17,17,31,0},
+                 {0,0,4,10,17,17,31,0},
         /* Ç */  {14,17,1,1,17,14,16,12},
         /* ü */  {10,0,17,17,17,17,30,0},
         /* é */  {24,0,14,17,31,1,30,0},
